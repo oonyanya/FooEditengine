b48842b65bb6d967ee81c471f26d3ad57b36c5b9
rirufa <test@yahoo.co.jp>
2025-04-08 22:59:26 +0900

    PageBound変更時だけAnchorIndexを保存するようにした

--- Core/Controller.cs
+++ Core/Controller.cs
@@ -1298,7 +1298,10 @@ namespace FooEditEngine
         {
             if (this.Document.LineBreak == LineBreakMethod.PageBound && this.View.PageBound.Width - this.View.LineBreakingMarginWidth > 0)
                 this.Document.PerformLayout();
+            //AnchorIndexが初期化されるので保存しておく
+            int anchorIndex = this.Document.AnchorIndex;
             this.AdjustCaret();
+            this.Document.AnchorIndex = anchorIndex;
         }
 
         void Document_Update(object sender, DocumentUpdateEventArgs e)
